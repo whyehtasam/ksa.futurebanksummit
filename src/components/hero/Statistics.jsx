2667ba697f16cce905e77ce65c8f9be2aa8ec8d9
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-27 03:52:44 +0530

    Add react-countup library for dynamic number animations

--- src/components/hero/Statistics.jsx
+++ src/components/hero/Statistics.jsx
@@ -1,37 +1,46 @@
 import React from "react";
+import CountUp from 'react-countup';
 
 const Statistics = () => {
   return (
     <div className="h-[642px] flex justify-center items-center border border-black">
       <div className="stats stats-vertical lg:stats-horizontal shadow btn-ghost no-animation hover:bg-none cursor-pointer">
 
-        <div className="stat text-center btn-ghost">
-          <div className="stat-value text-sky-600 text-6xl">31+</div>
-          <div className="stat-title font-bold text-2xl text-slate-50 p-4">
-            SPEAKERS
-          </div>
-        </div>
+      <div className="stat text-center btn-ghost">
+  <div className="stat-value text-sky-600 text-6xl">
+    <CountUp end={31} duration={5} />+
+  </div>
+  <div className="stat-title font-bold text-2xl text-slate-50 p-4">
+    SPEAKERS
+  </div>
+</div>
 
-        <div className="stat text-center">
-          <div className="stat-value text-sky-600 text-6xl">2 DAYS</div>
-          <div className="stat-title font-bold text-2xl text-slate-50 p-4">
-            EVENTS
-          </div>
-        </div>
+<div className="stat text-center">
+  <div className="stat-value text-sky-600 text-6xl">
+    <CountUp end={2} duration={5} /> DAYS
+  </div>
+  <div className="stat-title font-bold text-2xl text-slate-50 p-4">
+    EVENTS
+  </div>
+</div>
 
-        <div className="stat text-center">
-          <div className="stat-value text-sky-600 text-6xl">400+</div>
-          <div className="stat-title font-bold text-2xl text-slate-50 p-4">
-            DELEGATES
-          </div>
-        </div>
+<div className="stat text-center">
+  <div className="stat-value text-sky-600 text-6xl">
+    <CountUp end={400} duration={5} />+
+  </div>
+  <div className="stat-title font-bold text-2xl text-slate-50 p-4">
+    DELEGATES
+  </div>
+</div>
 
-        <div className="stat text-center">
-          <div className="stat-value text-sky-600 text-6xl">10+</div>
-          <div className="stat-title font-bold text-2xl text-slate-50 p-4">
-            HOURS OF NETWORKING
-          </div>
-        </div>
+<div className="stat text-center">
+  <div className="stat-value text-sky-600 text-6xl">
+    <CountUp end={10} duration={5} />+
+  </div>
+  <div className="stat-title font-bold text-2xl text-slate-50 p-4">
+    HOURS OF NETWORKING
+  </div>
+</div>
 
       </div>
     </div>
