ef4bba4eea7d731304f871a63603f3802e8a71f8
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-31 05:11:04 +0530

    Created partner and speaker components

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -1,24 +1,30 @@
 import Breadcrums from "../Breadcrums";
+import { imageData } from "./partnersData";
 const Partners = () => {
   return (
     <section className="partners">
       <Breadcrums heading="Partners" link="Partners" />
-      <div className="sechdule-content bg-white pt-20">
+      <div className="sechdule-content bg-slate-50 pt-20">
         <div className="container w-9/12 mx-auto p-3">
           <h2 className="text-5xl text-center font-bold font-oswald uppercase">
             EVENT PARTNERS
           </h2>
 
           <div className="my-16">
-            <h2 className="text-4xl text-orange-500 text-center font-bold font-roboto uppercase">
-              EVENT PARTNERS
-            </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div className="image flex-1 shadow-md">
+            {imageData.map((item, index) => (
+              <>
+                <h2 className="text-4xl text-orange-500 text-center font-bold font-roboto drop-shadow-sm mt-10 mb-4">
+                  {item.title}
+                </h2>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 
-                 <img src="" alt="" className="object-cover"/>
+                {item.path.map((item) =>  <div className="image flex-1 shadow-md my-8" key={index}>
+                    <img src={item} alt="img" className="object-cover" />
+                  </div>)}
+                 
                 </div>
-              </div>
+              </>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/components/partners/partnersData.js
@@ -0,0 +1,61 @@
+import img1 from './images/asset 1.png';
+import img2 from './images/asset 2.png';
+import img3 from './images/asset 3.png';
+import img4 from './images/asset 4.png';
+import img5 from './images/asset 5.png';
+import img6 from './images/asset 6.png';
+import img7 from './images/asset 7.png';
+import img8 from './images/asset 8.png';
+import img9 from './images/asset 9.png';
+import img10 from './images/asset 10.png';
+import img11 from './images/asset 11.png';
+import img12 from './images/asset 12.png';
+import img13 from './images/asset 13.png';
+import img14 from './images/asset 14.png';
+import img15 from './images/asset 15.png';
+import img16 from './images/asset 16.png';
+import img17 from './images/asset 17.png';
+import img18 from './images/asset 18.png';
+import img19 from './images/asset 19.png';
+import img20 from './images/asset 20.png';
+import img21 from './images/asset 21.png';
+import img22 from './images/asset 22.png';
+import img23 from './images/asset 23.png';
+import img24 from './images/asset 24.png';
+import img25 from './images/asset 25.png';
+import img26 from './images/asset 26.png';
+import img27 from './images/asset 27.png';
+import img28 from './images/asset 28.png';
+import img29 from './images/asset 29.png';
+import img30 from './images/asset 30.png';
+import img31 from './images/asset 31.png';
+import img32 from './images/asset 32.png';
+import img33 from './images/asset 33.png';
+import img34 from './images/asset 34.png';
+import img35 from './images/asset 35.png';
+import img36 from './images/asset 36.png';
+import img37 from './images/asset 37.png';
+import img38 from './images/asset 38.png';
+import img39 from './images/asset 39.png';
+import img40 from './images/asset 40.png';
+import img41 from './images/asset 42.png';
+import img42 from './images/asset 42.png';
+
+export const imageData = [
+    {
+        title:'Gold Partners',
+        path: [img1,img2,img3,img4]
+    },
+    {
+        title:'Bronze Partners',
+        path: [img4,img5,img6,img7]
+    },
+    {
+        title:'Networking Partners',
+        path: [img8,img9,img10,img11]
+    },
+    {
+        title:'Media Partners',
+        path: [img11,img12,img13,img14,img15,img16,img17,img18,img19,img20,img21,img22,img23,img24,img25,img26,img27,img28,img29,img30,img31,img32,img33,img34,img35,img36,img37,img38,img39,img40,img41,img42]
+    },
+];
