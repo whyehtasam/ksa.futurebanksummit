ebf4573b5eeef7d837c4fe4b84f9c9aabd47a793
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-31 04:37:53 +0530

    Refactor Partners and Speakers components

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -1,25 +1,29 @@
-import Breadcrums from "../Breadcrums"
+import Breadcrums from "../Breadcrums";
 const Partners = () => {
   return (
     <section className="partners">
-
-        <Breadcrums heading='Partners' link='Partners'/>
-        <div className="sechdule-content bg-white pt-20">
+      <Breadcrums heading="Partners" link="Partners" />
+      <div className="sechdule-content bg-white pt-20">
         <div className="container w-9/12 mx-auto p-3">
           <h2 className="text-5xl text-center font-bold font-oswald uppercase">
             EVENT PARTNERS
           </h2>
 
           <div className="my-16">
+            <h2 className="text-4xl text-orange-500 text-center font-bold font-roboto uppercase">
+              EVENT PARTNERS
+            </h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div className="image flex-1 shadow-md">
 
-            
-          
+                 <img src="" alt="" className="object-cover"/>
+                </div>
+              </div>
           </div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
--- src/components/speakers/Speakers.jsx
+++ src/components/speakers/Speakers.jsx
@@ -9,34 +9,34 @@ const Speakers = () => {
   return (
     <section className="speakers">
       <Breadcrums heading="Speakers" link="Speakers" />
-      <div className="sechdule-content bg-white pt-20">
-        <div className="container w-9/12 mx-auto p-3">
-          <h2 className="text-5xl text-center font-bold font-oswald">
-            EVENT SPEAKERS
-          </h2>
+        <div className="sechdule-content bg-white pt-20">
+          <div className="container w-9/12 mx-auto p-3">
+            <h2 className="text-5xl text-center font-bold font-oswald">
+              EVENT SPEAKERS
+            </h2>
 
-          <div className="my-16 grid lg:grid-cols-4 md:grid-cols-3 c gap-4">
+            <div className="my-16 grid lg:grid-cols-4 md:grid-cols-3 c gap-4">
 
-            {speakersData.map(({id,name,path,role,company}) => <div className="card card-compact flex-1 bg-base-100 shadow-xl hover:scale-[1.01] max-w-64 hover:drop-shadow-lg cursor-pointer" key={id}>
-              <figure className="">
-                <img
-                  src={path}
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="card-body flex justify-center items-center text-center tracking-wider">
-                <h2 className="card-title text-sky-700">{name}</h2>
-                <h2 className="card-title text-slate-500 text-base">{role}</h2>
-                <h2 className="card-title text-slate-600 text-base">{company}</h2>
-                <hr />
-                <div className="divider m-0 opacity-40 "></div> 
-                <FontAwesomeIcon icon={faLinkedinIn} className="hover:text-sky-600" size="2x" />
-              </div>
-            </div>)}
-          
+              {speakersData.map(({id,name,path,role,company}) => <div className="card card-compact flex-1 bg-base-100 shadow-xl hover:scale-[1.01] max-w-64 hover:drop-shadow-lg cursor-pointer" key={id}>
+                <figure className="">
+                  <img
+                    src={path}
+                    alt="Shoes"
+                  />
+                </figure>
+                <div className="card-body flex justify-center items-center text-center tracking-wider">
+                  <h2 className="card-title text-sky-700">{name}</h2>
+                  <h2 className="card-title text-slate-500 text-base">{role}</h2>
+                  <h2 className="card-title text-slate-600 text-base">{company}</h2>
+                  <hr />
+                  <div className="divider m-0 opacity-40 "></div> 
+                  <FontAwesomeIcon icon={faLinkedinIn} className="hover:text-sky-600" size="2x" />
+                </div>
+              </div>)}
+            
+            </div>
           </div>
         </div>
-      </div>
     </section>
   );
 };
