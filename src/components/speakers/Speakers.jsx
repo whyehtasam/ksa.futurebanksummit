ab72cb79052db90c25b3789b0fb5a909d3c31125
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-31 03:18:38 +0530

    Refactor Speakers component layout

--- src/components/speakers/Speakers.jsx
+++ src/components/speakers/Speakers.jsx
@@ -1,24 +1,97 @@
-import React from 'react'
-import Breadcrums from '../Breadcrums'
+import React from "react";
+import Breadcrums from "../Breadcrums";
 
 const Speakers = () => {
   return (
-    <section className='speakers'>
-    <Breadcrums heading='Speakers' link='Speakers'/>
-    <div className="sechdule-content bg-white pt-20">
+    <section className="speakers">
+      <Breadcrums heading="Speakers" link="Speakers" />
+      <div className="sechdule-content bg-white pt-20">
         <div className="container w-9/12 mx-auto p-3">
           <h2 className="text-5xl text-center font-bold font-oswald">
-          EVENT SPEAKERS
+            EVENT SPEAKERS
           </h2>
 
-          <div className="my-16">
-            
-            
+          <div className="my-16 grid grid-cols-4 gap-4">
+            <div className="card card-compact flex-1 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact flex-1 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact flex-1 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact flex-1 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact flex-1 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Speakers
+export default Speakers;
