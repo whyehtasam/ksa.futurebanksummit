0ca51eb1d5984b0b0220532f38bee4224fa72f4c
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-01 01:05:27 +0530

    Add image gallery with load more button

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import Breadcrums from "../Breadcrums";
 import GalleryHero from "./GalleryHero";
-
+import { imgData } from "./mediaData";
 const Gallery = () => {
   return (
     <section className="media w-full bg-white">
@@ -8,10 +8,16 @@ const Gallery = () => {
       <div className=" py-10 w-10/12 mx-auto">
         <GalleryHero />
         <div className="my-16 grid lg:grid-cols-4 md:grid-cols-3 c gap-4">
-          <div className="img flex-1">
-
-          </div>
+          {imgData.map((item, index) => (
+            <div className="img flex-1 shadow-md rounded-md hover:scale-[1.01] hover:drop-shadow-sm hover:-translate-y-2 transition-all duration-200 cursor-pointer overflow-hidden" key={index}>
+              <img src={item} alt="img" className="object-cover" />
+            </div>
+          ))}
         </div>
+        <div className="flex justify-center">
+          <button className="mx-auto outline-white drop-shadow-sm btn btn-wide bg-orange-500 text-white hover:text-black">Load more</button>
+        </div>
+        
       </div>
     </section>
   );
