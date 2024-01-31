78d1470985e484d25cd9cee5ce0ab8c627c21c6d
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-31 15:53:44 +0530

    Add ReactPlayer component to Awards page

--- src/components/awards/Awards.jsx
+++ src/components/awards/Awards.jsx
@@ -1,8 +1,24 @@
+import ReactPlayer from 'react-player';
+
 
 const Awards = () => {
   return (
-    <div>Awards</div>
-  )
-}
+    <section className="awards">
+      <div className="container">
+        <div className="award-video h-full">
+          <ReactPlayer
+            url="https://www.youtube.com/embed/qw_XbQ1EpKY?controls=0&rel=0&playsinline=1&enablejsapi=1&origin=https%3A%2F%2Fksa.futurebanksummit.com&widgetid=1"
+            playing='true'
+            width="100%"
+            height="100vh"
+            volume={0}
+            loop='true'
+          />
+        </div>
+        <div className="award-content"></div>
+      </div>
+    </section>
+  );
+};
 
-export default Awards
+export default Awards;
